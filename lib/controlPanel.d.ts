36073e703309f1645c5041db34f8ae0da7bf2466
Nikita <nzhdanov@forcode.pro>
2021-04-22 22:47:14 +0300

    first commit

---
+++ lib/controlPanel.d.ts
@@ -0,0 +1,30 @@
+declare const _default: {
+    controlPanelSections: {
+        label: string;
+        expanded: boolean;
+        controlSetRows: string[][];
+    }[];
+    controlOverrides: {
+        metric: {
+            label: string;
+            description: string;
+        };
+        secondary_metric: {
+            label: string;
+            default: null;
+            description: string;
+        };
+        color_scheme: {
+            description: string;
+        };
+        linear_color_scheme: {
+            description: string;
+        };
+        groupby: {
+            label: string;
+            description: string;
+        };
+    };
+};
+export default _default;
+//# sourceMappingURL=controlPanel.d.ts.map
